d58d5bd94d434d2258fecade21b61b7fb04eb056
Sandra Wazaz <sandrawazaz@gmail.com>
2021-01-04 21:06:14 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@
 # }
 
 def happy_birthday(birthday_kids)
-birthday_kids.each |name, age|
+birthday_kids.each do |name, age|
 puts "Happy Birthday #{name} you are now #{age} years old!"
-end 
+end
 end
