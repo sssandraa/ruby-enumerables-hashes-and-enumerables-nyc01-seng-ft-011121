7599f8d3e1c4a84885cd5d4478c95ed228a16bd9
Sandra Wazaz <sandrawazaz@gmail.com>
2021-01-04 20:58:12 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,4 +7,12 @@
 # }
 
 def select_winner(passengers)
-end 
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner
+
